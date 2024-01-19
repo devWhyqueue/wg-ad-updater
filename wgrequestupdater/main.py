5e174011158aeaa6211347224c164d700fc46296
Yannik Queisler <dev.yannik.queisler@gmail.com>
2024-01-19 09:17:25 +0100

    Add option exec_path

--- wgrequestupdater/main.py
+++ wgrequestupdater/main.py
@@ -2,7 +2,7 @@ import logging.config
 from importlib.resources import files
 from time import sleep
 
-from click import command, argument
+from click import command, argument, option
 from selenium.common.exceptions import WebDriverException
 
 from wgrequestupdater.webdriver import web_driver
@@ -15,11 +15,12 @@ log = logging.getLogger("wgrequestupdater.main")
 @command()
 @argument('username')
 @argument('password')
-def cli(username, password):
+@option('--exec-path', '-e', default=None, help="Path to the geckodriver executable.")
+def cli(username, password, exec_path):
     from wgrequestupdater.updater import WgRequestUpdater
     log.info("WG Request updater successfully started.")
     while True:
-        webdriver = web_driver()
+        webdriver = web_driver(exec_path)
         req = WgRequestUpdater(webdriver)
         try:
             req.update(username, password)
--- wgrequestupdater/webdriver.py
+++ wgrequestupdater/webdriver.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.webdriver import WebDriver
 
 
-def web_driver() -> WebDriver:
+def web_driver(exec_path: str = None) -> WebDriver:
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--width=1920")
     options.add_argument("--height=1080")
-    driver = webdriver.Firefox(options=options)
+
+    if exec_path:
+        service = Service(exec_path)
+        driver = webdriver.Firefox(service=service, options=options)
+    else:
+        driver = webdriver.Firefox(options=options)
+
     driver.implicitly_wait(3)
     return driver
